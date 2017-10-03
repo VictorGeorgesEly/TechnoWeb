aa7173eb206f3945adbe8c4aa2150e17208e44db
chel723 <chel723@gmail.com>
2017-10-03 17:45:42 +0200

    add second exercice

---
+++ src/main/java/dblab/hello/Log4jBasics.java
@@ -0,0 +1,14 @@
+package ex01C;
+import org.apache.log4j.*;
+public class Log4jBasics
+{
+  protected static Logger log =
+Logger.getLogger(Log4jBasics.class);
+
+public static void main(String[] args)
+  {
+  	log.info("INFO: Cool !");
+    log.debug("DEBUG: Cool !");
+    
+  }
+}
