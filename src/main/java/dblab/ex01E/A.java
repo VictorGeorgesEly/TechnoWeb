6b9cdd3c19cfbc14cb506a3773fda0bdf284fc43
Victor Ely <vely@juniorisep.com>
2017-10-07 15:06:32 +0200

    Add all exercice

--- src/main/java/dblab/ex01E/A.java
+++ src/main/java/dblab/ex01E/A.java
@@ -4,32 +4,29 @@ class A {
     int x;
 
     void m() {
-        System.out.println("Je suis dans la méthode m d'une instance de A");
+        System.out.println("Je suis dans la méthode m d'une instance de A");
     }
 }
 
 class B extends A {
-    int x;
 
     void m() {
-        System.out.println("Je suis dans la méthode m d'une instance de B");
+        System.out.println("Je suis dans la méthode m d'une instance de B");
     }
 }
 
 class C extends B {
-    int x, a;
+    int a;
 
     void m() {
-        System.out.println("Je suis dans la méthode m d'une instance de A");
+        System.out.println("Je suis dans la méthode m d'une instance de C");
     }
 
     void test() {
         a = super.x;
-        a = super.super.x;
         a = ((B) this).x;
         a = ((A) this).x;
         super.m();
-        super.super.m();
         ((B) this).m();
     }
 }
